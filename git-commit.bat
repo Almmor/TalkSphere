f09c5aed675491f4c3ee61cd9c5e3cf1440550a7
林间雨 <almmor@163.com>
2026-05-09 14:01:21 +0800

    ci: 移除Android构建，只打包Windows/macOS/Linux

--- git-commit.bat
+++ git-commit.bat
@@ -6,8 +6,8 @@ echo [1/2] 添加更改...
 git add .
 
 echo [2/2] 提交并推送...
-git commit -m "ci: 添加自动构建触发 - push到main/master分支时自动构建"
+git commit -m "ci: 移除Android构建，只打包Windows/macOS/Linux"
 git push origin master
 
-echo 完成！GitHub Actions将自动触发构建。
+echo 完成！GitHub Actions将只构建桌面端。
 pause
